5b5d31b3852544fe0dce3a8ce7617ab5071e947a
zMendes <leonardom1808@gmail.com>
2020-06-13 17:48:13 -0300

    xD

---
+++ server.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, render_template, request, jsonify
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+
+@app.route('/')
+def control():
+   return render_template('index.html')
+
+@app.route('/status', methods = ['POST', 'GET'])
+def status():
+   global led
+   global but
+   global ts
+   global id
+   global pot
+   if request.method == 'POST':
+      print("Entei no post: {0}".format(request.form['TS']))
+      status = request.form
+      led = status['LED']
+      id = status['ID']
+      but = status['BUT']
+      ts = status['TS'][0:19]
+      pot = status['POT'][0:4]
+      #return render_template("status.html", status = status)
+   return jsonify({'led' : led,'but':but,"id":id,"ts":ts, "pot":pot}),200
+
+if __name__ == '__main__':
+   port = int(os.environ.get("PORT", 5000))
+   app.run(host='0.0.0.0', debug=True,port=port)
