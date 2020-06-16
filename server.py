eb6ff5fb446645230acf25080ac4388baafb983e
Ubuntu <ubuntu@ip-172-31-24-5.sa-east-1.compute.internal>
2020-06-16 16:35:42 +0000

    retirando led

--- server.py
+++ server.py
@@ -8,19 +8,18 @@ app = Flask(__name__)
 def control():
    return render_template('index.html')
 
-@app.route('/status', methods = ['POST', 'GET'])
+@app.route('/home/status', methods = ['POST', 'GET'])
 def status():
-   global led, but, id, ts, pot
+   global but, id, ts, pot
    if request.method == 'POST':
       print("Entei no post: {0}".format(request.form['TS']))
       status = request.form
-      led = status['LED']
       id = status['ID']
       but = status['BUT']
-      ts = status['TS'][0:19]
+      ts = status['TS'][0:22]
       pot = status['POT'][0:4]
       #return render_template("status.html", status = status)
-   return jsonify({'led' : led,'but':but,"id":id,"ts":ts, "pot":pot}),200
+   return jsonify({"id":id,"ts":ts,"but":but, "pot":pot}),200
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', debug=True,port=80)
