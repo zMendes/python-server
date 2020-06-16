205ab45c4d6b9106605a10debe447e4c36510a29
zMendes <leonardom1808@gmail.com>
2020-06-16 15:35:18 -0300

    Vai funcionar confia

--- server.py
+++ server.py
@@ -3,23 +3,36 @@ from flask import Flask, render_template, request, jsonify
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
-
+global but0 = 0  
+global id0= 0
+global ts0 = "2020-01-01-00:00:00GMT" 
+global pot0 = 0
+global but1=0 
+global id1 = 
+global ts1 = "2020-01-01-00:00:00GMT" 
+global pot1 = 0
 @app.route('/')
 def control():
    return render_template('index.html')
 
 @app.route('/home/status', methods = ['POST', 'GET'])
 def status():
-   global but, id, ts, pot
+
    if request.method == 'POST':
       print("Entei no post: {0}".format(request.form['TS']))
-      status = request.form
-      id = status['ID']
-      but = status['BUT']
-      ts = status['TS'][0:22]
-      pot = status['POT'][0:4]
-      #return render_template("status.html", status = status)
-   return jsonify({"id":id,"ts":ts,"but":but, "pot":pot}),200
+      if(request.form['ID']==0):
+         status = request.form
+         id0 = status['ID']
+         but0 = status['BUT']
+         ts0 = status['TS'][0:22]
+         pot0 = status['POT'][0:4]
+      else:
+         id1 = status['ID']
+         but1 = status['BUT']
+         ts1 = status['TS'][0:22]
+         pot1 = status['POT'][0:4]
+
+   return jsonify({"id":id0,"ts":ts0,"but":but0, "pot":pot0},{"id":id1,"ts":ts1,"but":but1, "pot":pot1}),200
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', debug=True,port=80)
